35326c590bd19e8a8a9a8b2f58c577cd72722ba7
Com1 Software, Inc. <information@com1software.com>
2026-09-23 17:44:14 -0400

    Delete mini-db/target directory

--- mini-db/target/debug/deps/mini_db.d
+++
@@ -1,5 +0,0 @@
-C:\Rust-Examples\mini-db\target\debug\deps\mini_db.d: src\main.rs
-
-C:\Rust-Examples\mini-db\target\debug\deps\mini_db.exe: src\main.rs
-
-src\main.rs:
--- mini-db/target/debug/mini-db.d
+++
@@ -1,1 +0,0 @@
-C:\Rust-Examples\mini-db\target\debug\mini-db.exe: C:\Rust-Examples\mini-db\src\main.rs
