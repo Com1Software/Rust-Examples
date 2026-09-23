6b4bb1ec610b6445e78fce07d161ed6e9a899c93
Com1 Software, Inc. <information@com1software.com>
2026-09-23 17:38:01 -0400

    Delete hello/target directory

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-C:\Rust-Examples\hello\target\debug\deps\hello.d: src\main.rs
-
-C:\Rust-Examples\hello\target\debug\deps\hello.exe: src\main.rs
-
-src\main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-C:\Rust-Examples\hello\target\debug\hello.exe: C:\Rust-Examples\hello\src\main.rs
