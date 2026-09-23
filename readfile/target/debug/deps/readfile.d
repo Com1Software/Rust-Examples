4e2c7bcd9a2aff5018dc51aad3db680362b7b4cf
Lenovo 15 <information@com1software.com>
2026-09-23 17:46:19 -0400

    added

---
+++ readfile/target/debug/deps/readfile.d
@@ -0,0 +1,5 @@
+C:\Rust-Examples\readfile\target\debug\deps\readfile.d: src\main.rs
+
+C:\Rust-Examples\readfile\target\debug\deps\readfile.exe: src\main.rs
+
+src\main.rs:
---
+++ readfile/target/debug/readfile.d
@@ -0,0 +1,1 @@
+C:\Rust-Examples\readfile\target\debug\readfile.exe: C:\Rust-Examples\readfile\src\main.rs
